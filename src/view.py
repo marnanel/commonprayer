c5dd3c64015232dc32405534537fa1886f3e0956
Thomas Thurman <tthurman@gnome.org>
2011-01-17 19:56:07 -0500

    rm debug

--- src/view.py
+++ src/view.py
@@ -10,7 +10,5 @@ class CommonPrayerWindow(QMainWindow):
 		self._html = QTextBrowser()
 		self.setCentralWidget(self._html)
 
-		self._html.setHtml('This <i>is</i> nice.')
-
 	def setHtml(self, html):
 		self._html.setHtml(html)
