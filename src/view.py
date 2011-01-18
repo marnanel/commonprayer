34ef920c28952346924d0d43b14781a58d78019c
Thomas Thurman <tthurman@gnome.org>
2011-01-17 20:04:40 -0500

    previous/next controls

--- src/view.py
+++ src/view.py
@@ -10,5 +10,29 @@ class CommonPrayerWindow(QMainWindow):
 		self._html = QTextBrowser()
 		self.setCentralWidget(self._html)
 
+		self._prev = QAction("<-", self)
+		self._next = QAction("->", self)
+
+		self.connect(self._prev,
+			SIGNAL('triggered()'),
+			self,
+			SLOT('goPrevious()'))
+
+		self.connect(self._next,
+			SIGNAL('triggered()'),
+			self,
+			SLOT('goNext()'))
+
+		self.menuBar().addAction(self._prev)
+		self.menuBar().addAction(self._next)
+
+	def goPrevious(self):
+		self.emit(SIGNAL('move(QString)'),
+			'previous')
+
+	def goNext(self):
+		self.emit(SIGNAL('move(QString)'),
+			'next')
+
 	def setHtml(self, html):
 		self._html.setHtml(html)
